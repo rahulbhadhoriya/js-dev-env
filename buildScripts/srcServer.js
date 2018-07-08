6ca2be9865bfcd41343040d956604924260cb1f2
Rahul bhadhoriya <rahulbhadhoriya302@gmail.com>
2018-07-08 16:16:28 +0530

    npm scripts for automation

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+
+let port = 3000;
+const app = express();
+
+// default route & send index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+//port number
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+})
